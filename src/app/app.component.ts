e6580922ae7a69e0a0fc7b8b9da3dd0f4b513573
christianpinheiro <93pinheiro@gmail.com>
2022-03-26 00:05:54 -0300

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,31 @@
 import { Component } from '@angular/core';
 
+interface Tarefas {
+  v: string;
+}
+
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
-  
-  
-  add(){
+export class AppComponent {
+  n: number;
+  v: string;
+  list: Array<Tarefas> = [];
 
+  constructor() {
+   
   }
+
+  add(v: string) {
+    this.list.push({ v });
+  }
+
+  getList() {
+    
+    return this.list;
+  }
+
+
 }
